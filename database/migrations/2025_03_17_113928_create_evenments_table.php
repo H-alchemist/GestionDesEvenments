9038a3515a3603de674ff91f725fc7d56fe8b8c2
hamza <echchablihamza1@gmail.com>
2025-03-17 11:43:19 +0000

    migrations

---
+++ database/migrations/2025_03_17_113928_create_evenments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evenments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('date');
+            $table->string('location');
+            $table->string('image')->nullable();
+            $table->integer('capacity');
+            $table->boolean('state')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evenments');
+    }
+};
